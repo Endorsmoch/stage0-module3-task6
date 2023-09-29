bbd10e517465413bab78748be5b2d5bfd0d8b517
Endorsmoch <a16003872@alumnos.uady.mx>
2023-09-29 10:06:04 -0600

    Finished

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int roundedNum = (int) Math.round((double) numberToBeRounded);
+        System.out.println(roundedNum);
     }
 }
